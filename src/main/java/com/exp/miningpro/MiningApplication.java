a9092f438fe3c527580fcc8429225046928fb7a6
Rohit <rohitkumar4praja@gmail.com>
2026-05-01 12:22:26 +0530

    first commit

---
+++ src/main/java/com/exp/miningpro/MiningApplication.java
@@ -0,0 +1,12 @@
+package com.exp.miningpro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiningApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiningApplication.class, args);
+    }
+}
